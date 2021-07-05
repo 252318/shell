b75b438866da70b3f86a376c5465e3b588a62679
FanJialins <86670875+FanJialins@users.noreply.github.com>
2021-07-06 02:06:11 +0800

    代码复写，函数封装。

--- fucknetwork.sh
+++ fucknetwork.sh
@@ -18,13 +18,15 @@
 
 LANG=en_US.UTF-8
 
-VAR_LOCAL_NETWORK_STATUS=false
-VAR_GATEWAY_STATUS=false
-VAR_WAN_STATUS=false
-VAR_DNS_STATUS=false
-VAR_IP_ADDRESS_ONOFF=off
-VAR_IPV4_ADDRESS_STATUS=false
-VAR_IPV6_ADDRESS_STATUS=false
+VAR_STATUS_LOCAL_NETWORK=BAD
+VAR_STATUS_GATEWAY=BAD
+VAR_STATUS_WAN=BAD
+VAR_STATUS_DNS=BAD
+VAR_ONOFF_IP_ADDRESS=off
+VAR_IPV4_ADDRESS=NONE
+VAR_IPV6_ADDRESS=NONE
+VAR_TIME_PING_LAN=TIMEOUT
+VAR_TIME_PING_WAN=TIMEOUT
 VAR_REMOTE_IP=119.29.29.29
 VAR_REMOTE_DOMAIN=www.qq.com
 VAR_CURL_IPV4=ipv4.ip.sb
@@ -35,10 +37,14 @@ VAR_CURL_IPV6=ipv6.ip.sb
 ##################
 
 COLOR_RED="\033[1;31m"
+COLOR_REDS="\033[7;31m"
 COLOR_GREEN="\033[1;32m"
+COLOR_GREENS="\033[7;32m"
 COLOR_YELLOW="\033[1;33m"
 COLOR_BLUE="\033[1;34m"
+COLOR_BLUES="\033[7;34m"
 COLOR_PURPLE="\033[1;35m"
+COLOR_WHITE="\033[1;37m"
 COLOR_RESET="\033[0m"
 
 STRING_1="LOCAL NETWORK:              "
@@ -54,127 +60,126 @@ PADDING_A="                                                                 LAN 
 PADDING_B="                                                                 WAN Ping:"
 PADDING_Y="**************$COLOR_YELLOW Hosts Scanning started at: $(date "+%a %d %b %Y %I:%M:%S %p %Z") $COLOR_RESET**************"
 
-echo "$PADDING_X"
+PRINT_PADDING_X(){
+    echo "$PADDING_X"
+}
 
-###################
-# DATA and STATUS #
-###################
+##########################
+# NETWORK DATA && STATUS #
+##########################
 
-ip r &>/dev/null
+NETWORK_DATA_STATUS(){
+    COLOR_LOCAL_NETWORK=$COLOR_RED
+    COLOR_GATEWAY=$COLOR_RED
+    COLOR_WAN=$COLOR_RED
+    COLOR_DNS=$COLOR_RED
+    
+    ip r &>/dev/null
 
-# $ curl --remote-name -L https://github.com/brona/iproute2mac/raw/master/src/ip.py
-# $ chmod +x ip.py
-# $ mv ip.py /usr/local/bin/ip
+    # $ curl --remote-name -L https://github.com/brona/iproute2mac/raw/master/src/ip.py
+    # $ chmod +x ip.py
+    # $ mv ip.py /usr/local/bin/ip
 
-if [ $? -eq 0 ]; then
-    VAR_LOCAL_NETWORK_STATUS="true"
-    LOCAL_IP_TMP1=$(ip a) && LOCAL_IP_TMP2=${LOCAL_IP_TMP1##*inet } && LOCAL_IP_MASK=${LOCAL_IP_TMP2%% brd*} && LOCAL_IP=${LOCAL_IP_MASK%/*}
-    NETMASK_VAR=${LOCAL_IP_MASK#*/}
-    GATEWAY_IP_TMP1=$(ip r) && GATEWAY_IP_TMP2=${GATEWAY_IP_TMP1#*via } && GATEWAY_IP=${GATEWAY_IP_TMP2%% dev*}
-    GATEWAY_NAME_TMP1=$(nslookup "$GATEWAY_IP") && GATEWAY_NAME_TMP2=${GATEWAY_NAME_TMP1#*\=\ } && GATEWAY_NAME_VAR=${GATEWAY_NAME_TMP2%.*}
+    if [ $? -eq 0 ]; then
+        VAR_STATUS_LOCAL_NETWORK=OK && COLOR_LOCAL_NETWORK=$COLOR_BLUE
+        LOCAL_IP_TMP1=$(ip a) && LOCAL_IP_TMP2=${LOCAL_IP_TMP1##*inet } && LOCAL_IP_MASK=${LOCAL_IP_TMP2%% brd*} && LOCAL_IP=${LOCAL_IP_MASK%/*}
+        NETMASK_VAR=${LOCAL_IP_MASK#*/}
+        GATEWAY_IP_TMP1=$(ip r) && GATEWAY_IP_TMP2=${GATEWAY_IP_TMP1#*via } && GATEWAY_IP=${GATEWAY_IP_TMP2%% dev*}
+        GATEWAY_NAME_TMP1=$(nslookup "$GATEWAY_IP") && GATEWAY_NAME_TMP2=${GATEWAY_NAME_TMP1#*\=\ } && GATEWAY_NAME_VAR=${GATEWAY_NAME_TMP2%.*}
 
-    ping -c1 "$GATEWAY_IP" &>/dev/null && VAR_GATEWAY_STATUS=true
-    ping -c1 $VAR_REMOTE_IP &>/dev/null && VAR_WAN_STATUS=true
-    nslookup $VAR_REMOTE_DOMAIN &>/dev/null && VAR_DNS_STATUS=true && VAR_IP_ADDRESS_ONOFF=on
+        ping -c1 "$GATEWAY_IP" &>/dev/null && VAR_STATUS_GATEWAY=OK && COLOR_GATEWAY=$COLOR_BLUE
+        ping -c1 $VAR_REMOTE_IP &>/dev/null && VAR_STATUS_WAN=OK && COLOR_WAN=$COLOR_BLUE
+        nslookup $VAR_REMOTE_DOMAIN &>/dev/null && VAR_STATUS_DNS=OK && VAR_ONOFF_IP_ADDRESS=on && COLOR_DNS=$COLOR_BLUE
+    fi
+}
 
-fi
+NETWORK_DATA_STATUS
 
-###################
+PRINT_NETWORK_DATA_STATUS(){
+    echo -e "$STRING_1 $COLOR_LOCAL_NETWORK $VAR_STATUS_LOCAL_NETWORK $COLOR_YELLOW $LOCAL_IP/$NETMASK_VAR via gateway $GATEWAY_IP($GATEWAY_NAME_VAR). $COLOR_RESET"
+    echo -e "$STRING_2 $COLOR_GATEWAY $VAR_STATUS_GATEWAY $COLOR_RESET"
+    echo -e "$STRING_3 $COLOR_WAN $VAR_STATUS_WAN $COLOR_RESET"
+    echo -e "$STRING_4 $COLOR_DNS $VAR_STATUS_DNS $COLOR_RESET"
+}
 
-#################
-# Network check	#
-#################
-
-if [[ $VAR_LOCAL_NETWORK_STATUS = true ]]; then
-    echo -e "$STRING_1 $COLOR_YELLOW $LOCAL_IP/$NETMASK_VAR via gateway $GATEWAY_IP($GATEWAY_NAME_VAR). $COLOR_RESET"
-else
-    echo -e "$STRING_1 $COLOR_RED BAD $COLOR_RESET"
-fi
-
-if [[ $VAR_GATEWAY_STATUS = true ]]; then
-    echo -e "$STRING_2 $COLOR_BLUE OK $COLOR_RESET"
-else
-    echo -e "$STRING_2 $COLOR_RED BAD $COLOR_RESET"
-fi
-
-
-if [[ $VAR_WAN_STATUS = true ]]; then
-    echo -e "$STRING_3 $COLOR_BLUE OK $COLOR_RESET"
-else
-    echo -e "$STRING_3 $COLOR_RED BAD $COLOR_RESET"
-fi
-
-if [[ $VAR_DNS_STATUS = true ]]; then
-    echo -e "$STRING_4 $COLOR_BLUE OK $COLOR_RESET"
-else
-    echo -e "$STRING_4 $COLOR_RED BAD $COLOR_RESET"
-fi
-
-#################
+##########################
 
 ####################
-# IP address check #
+# IP DATA & STATUS #
 ####################
 
-if [[ $VAR_IP_ADDRESS_ONOFF = on ]];then
-    curl -s $VAR_CURL_IPV4 &>/dev/null && VAR_IPV4_ADDRESS_STATUS=true && VAR_IPV4_ADDRESS=$(curl -s $VAR_CURL_IPV4) 
-    curl -s $VAR_CURL_IPV6 &>/dev/null && VAR_IPV6_ADDRESS_STATUS=true && VAR_IPV6_ADDRESS=$(curl -s $VAR_CURL_IPV6) 
-fi
-
-if [[ $VAR_IPV4_ADDRESS_STATUS = true ]];then
-    echo -e "$STRING_5  $VAR_IPV4_ADDRESS"
-else
-    echo -e "$STRING_5 $COLOR_RED BAD $COLOR_RESET"
-fi
-
-if [[ $VAR_IPV6_ADDRESS_STATUS = true ]];then
-    echo -e "$STRING_6  $VAR_IPV6_ADDRESS"
-else
-    echo -e "$STRING_6 $COLOR_RED BAD $COLOR_RESET"
-fi
-
+PRINT_IP_DATA_STATUS(){
+    COLOR_IPV4=$COLOR_RED
+    COLOR_IPV6=$COLOR_RED
+    if [[ $VAR_ONOFF_IP_ADDRESS = on ]];then
+        curl -s $VAR_CURL_IPV4 &>/dev/null && VAR_IPV4_ADDRESS=$(curl -s "$VAR_CURL_IPV4") && COLOR_IPV4=$COLOR_WHITE
+        curl -s $VAR_CURL_IPV6 &>/dev/null && VAR_IPV6_ADDRESS=$(curl -s "$VAR_CURL_IPV6") && COLOR_IPV6=$COLOR_WHITE
+    fi
+    echo -e "$STRING_5 $COLOR_IPV4 $VAR_IPV4_ADDRESS $COLOR_RESET"
+    echo -e "$STRING_6 $COLOR_IPV6 $VAR_IPV6_ADDRESS $COLOR_RESET"
+}
 ####################
 
-###############
-# Hosts check #
-###############
+######################
+# HOSTS ALIVE in LAN #
+######################
 
-echo -e "$PADDING_Y"
-if [[ $VAR_LOCAL_NETWORK_STATUS = true ]]; then GATEWAY_IP_PREFIX=${GATEWAY_IP%.*}; fi
-for ((i = 1; i <= 254; i++)); do
-    {
-        HOST_IP=$GATEWAY_IP_PREFIX.$i
-        ping -c1 "$HOST_IP" &>/dev/null
-        if [ $? -eq 0 ]; then
-            HOSTNAME_TMP1=$(nslookup "$HOST_IP") && HOSTNAME_TMP2=${HOSTNAME_TMP1#*\=\ } && HOSTNAME_VAR=${HOSTNAME_TMP2%.*}
-            echo -e "$COLOR_YELLOW $(date "+%a %d %b %Y %I:%M:%S %p %Z"):        $HOST_IP($HOSTNAME_VAR) $COLOR_RESET"
-        fi
-    } &
-done
-wait &>/dev/null
+PRINT_HOST_ALIVE(){
+    echo -e "**************$COLOR_YELLOW Hosts Scanning started at: $(date "+%a %d %b %Y %I:%M:%S %p %Z") $COLOR_RESET**************"
+    if [[ $VAR_STATUS_LOCAL_NETWORK = OK ]]; then GATEWAY_IP_PREFIX=${GATEWAY_IP%.*}; fi
+    for ((i = 1; i <= 254; i++)); do
+        {
+            HOST_IP=$GATEWAY_IP_PREFIX.$i
+            ping -c1 "$HOST_IP" &>/dev/null
+            if [ $? -eq 0 ]; then
+                HOSTNAME_TMP1=$(nslookup "$HOST_IP") && HOSTNAME_TMP2=${HOSTNAME_TMP1#*\=\ } && HOSTNAME_VAR=${HOSTNAME_TMP2%.*}
+                echo -e "$COLOR_YELLOW $(date "+%a %d %b %Y %I:%M:%S %p %Z"):        $HOST_IP($HOSTNAME_VAR) $COLOR_RESET"
+            fi
+        } &
+    done
+    wait &>/dev/null
+}
 
 ###############
 
-#############
-# Speed Test#
-#############
+########################
+# SPEED DATA && STATUS #
+########################
 
-# echo -e "$PADDING_Z"
-echo "$STRING_7"
-if [[ $VAR_GATEWAY_STATUS = true ]]; then
-    LAN_TIMEOUT1="$(ping -c4 "$GATEWAY_IP")" && LAN_TIMEOUT2=${LAN_TIMEOUT1##*=} && LAN_TIMEOUT3=${LAN_TIMEOUT2#*/} && LAN_TIMEOUT=${LAN_TIMEOUT3%%/*}
-    echo "$PADDING_A $LAN_TIMEOUT ms"
-else
-    echo -e "$PADDING_A $COLOR_RED TIMEOUT $COLOR_RESET"
-fi
+PRINT_SPEED_DATA_STATUS(){
+    COLOR_PING_LAN=$COLOR_RED
+    COLOR_PING_WAN=$COLOR_RED
+    echo "$STRING_7"
+    if [[ $VAR_STATUS_GATEWAY = OK ]]; then
+        LAN_TIMEOUT1="$(ping -c4 "$GATEWAY_IP")" && LAN_TIMEOUT2=${LAN_TIMEOUT1##*=} && LAN_TIMEOUT3=${LAN_TIMEOUT2#*/} && \
+        VAR_TIME_PING_LAN="${LAN_TIMEOUT3%%/*} ms" && COLOR_PING_LAN=$COLOR_WHITE
+    fi
 
-if [[ $VAR_WAN_STATUS = true ]]; then
-    WAN_TIMEOUT1="$(ping -c4 "$VAR_REMOTE_IP")" && WAN_TIMEOUT2=${WAN_TIMEOUT1##*=} && WAN_TIMEOUT3=${WAN_TIMEOUT2#*/} && WAN_TIMEOUT=${WAN_TIMEOUT3%%/*}
-    echo "$PADDING_B $WAN_TIMEOUT ms"
-else
-    echo -e "$PADDING_B $COLOR_RED TIMEOUT $COLOR_RESET"
-fi
+    if [[ $VAR_STATUS_WAN = OK ]]; then
+        WAN_TIMEOUT1="$(ping -c4 "$VAR_REMOTE_IP")" && WAN_TIMEOUT2=${WAN_TIMEOUT1##*=} && WAN_TIMEOUT3=${WAN_TIMEOUT2#*/} && \
+        VAR_TIME_PING_WAN="${WAN_TIMEOUT3%%/*} ms" && COLOR_PING_WAN=$COLOR_WHITE
+    fi
+    echo -e "$PADDING_A $COLOR_PING_LAN $VAR_TIME_PING_LAN $COLOR_RESET"
+    echo -e "$PADDING_B $COLOR_PING_WAN $VAR_TIME_PING_WAN $COLOR_RESET"
+}
 
-#############
+########################
 
-echo "$PADDING_X"
+
+#######################
+# PRINT Network CHECK #
+#######################
+
+PRINT_NETWORK_HEALTH_STATUS(){
+    PRINT_PADDING_X
+    PRINT_NETWORK_DATA_STATUS
+    PRINT_IP_DATA_STATUS
+    PRINT_HOST_ALIVE
+    PRINT_SPEED_DATA_STATUS
+    PRINT_PADDING_X
+}
+
+#######################
+
+RINT_NETWORK_HEALTH_STATUS
+
